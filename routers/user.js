a05f7f874dd6a5ec2b914e6522bed53695a62e45
Alex Rosario <rosarionolascoalexjunior@gmail.com>
2024-02-22 15:34:30 -0400

    User route and middleware done

---
+++ routers/user.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const User = require("./models/User");
+
+const router = new express.Router();
+
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.post("/users/logout", Auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.token.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+router.post("/users/logout", Auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
